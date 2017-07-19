00fb3bf366a722043fb9569c6243c8bc014ea38f
Brennan Fee <github@todaytechsoft.biz>
2017-07-18 23:00:37 -0500

    Added the C# fruit example.

---
+++ csharp/FruitCodingExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace FruitCodingExample
+{
+    public abstract class Fruit
+    {
+        public virtual void GetColor()
+        {
+            Console.Write("Unknown");
+        }
+    }
+
+    public class Apple : Fruit
+    {
+        public override void GetColor()
+        {
+            Console.WriteLine("Red");
+        }
+    }
+
+    public class Banana : Fruit
+    {
+        public new void GetColor()
+        {
+            Console.WriteLine("Yellow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple a = new Apple();
+            Banana b = new Banana();
+
+            a.GetColor();
+            b.GetColor();
+
+            DisplayFruitColor(a);
+            DisplayFruitColor(b);
+        }
+
+        private static void DisplayFruitColor(Fruit f)
+        {
+            f.GetColor();
+        }
+    }
+}
